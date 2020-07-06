20d2b20dd0d408b66b6ecf8c1575a8d5cf60e82a
Seyed Ali Roshan <Seyedalir@yahoo.com>
2020-07-07 03:37:10 +0430

    a bit improvments

--- src/app/share/db.service.ts
+++ src/app/share/db.service.ts
@@ -1,13 +1,13 @@
-import { Keyword } from './models/keyword';
-import { Cookie } from './models/cookie';
-import { DB } from './models/db';
-import { CMS } from './models/cms';
-import { El } from './models/el';
-import { Injectable, NgZone } from '@angular/core';
-import { existsSync, readFileSync, writeFile, mkdir, mkdirSync, writeFileSync } from 'fs';
-import { JsonConvert, ValueCheckingMode } from 'json2typescript';
+import {Keyword} from './models/keyword';
+import {Cookie} from './models/cookie';
+import {DB} from './models/db';
+import {CMS} from './models/cms';
+import {El} from './models/el';
+import {Injectable, NgZone} from '@angular/core';
+import {existsSync, mkdir, mkdirSync, readFileSync, writeFile, writeFileSync} from 'fs';
+import {JsonConvert, ValueCheckingMode} from 'json2typescript';
 import * as os from 'os';
-import { v4 } from 'uuid';
+import {v4} from 'uuid';
 import * as clone from 'clone';
 
 @Injectable()
@@ -15,13 +15,25 @@ export class DbService {
     constructor(
         private _zone: NgZone
     ) {
-        const separator = this.getSeparator();
-        this.dbLocation = `${os.homedir()}${separator}.cms-detector${separator}db.json`;
+        const separator = DbService.getSeparator();
+        DbService.dbLocation = `${os.homedir()}${separator}.cms-detector${separator}db.json`;
     }
 
     private static dbLocation: string;
     private static db: DB;
     private static jsonConvert: JsonConvert;
+
+    private static writeSync(path: string, str: string) {
+        writeFileSync(path, str, { encoding: 'utf8', flag: 'w' });
+    }
+
+    private static errorAlert() {
+        alert('there is an error when we try to create ur databse');
+    }
+
+    private static getSeparator() {
+        return os.platform() === 'win32' ? '\\' : '/';
+    }
 
     get jConverter(): JsonConvert {
         let jConverter = DbService.jsonConvert;
@@ -44,10 +56,6 @@ export class DbService {
 
     get dbLocation(): string {
         return DbService.dbLocation;
-    }
-
-    set dbLocation(dbLocation: string) {
-        DbService.dbLocation = dbLocation;
     }
 
     deleteCMSFormDB(cms: CMS): DB {
@@ -88,7 +96,7 @@ export class DbService {
         let db = this.db;
 
         // because the cms is a clone of the real one we can't use indexOf or any feature
-        // that dependes on refrences, so lets do it in the old fashioned way :D
+        // that depends on references, so lets do it in the old fashioned way :D
         let index = -1;
 
         for (let i = 0; i < db.cmses.length; i++) {
@@ -125,9 +133,7 @@ export class DbService {
         const path = this.dbLocation;
         const str = readFileSync(path, { encoding: 'utf8' });
         const json = JSON.parse(str);
-        const db = this.jConverter.deserialize(json, DB) as DB;
-
-        this.db = db;
+        this.db = this.jConverter.deserialize(json, DB) as DB;
     }
 
     private generateAndLoadDB(async = false, database?: DB) {
@@ -168,7 +174,7 @@ export class DbService {
     // this one is a little messy | sorry :D
     private writeOnDB(str: string, async = false) {
         const path = this.dbLocation;
-        const separator = this.getSeparator();
+        const separator = DbService.getSeparator();
         const arr = path.split(separator);
         arr.splice(arr.length - 1, 1);
         const directory = arr.join(separator);
@@ -179,7 +185,7 @@ export class DbService {
             if (!existed) {
                 mkdir(directory, { recursive: true }, (err) => {
                     if (err) {
-                        this.errorAlert();
+                        DbService.errorAlert();
                         return;
                     }
                     this.writeAsync(path, str);
@@ -191,12 +197,12 @@ export class DbService {
             try {
                 if (!existed) {
                     mkdirSync(directory, { recursive: true });
-                    this.writeSync(path, str);
+                    DbService.writeSync(path, str);
                 } else {
-                    this.writeSync(path, str);
+                    DbService.writeSync(path, str);
                 }
             } catch (e) {
-                this.errorAlert();
+                DbService.errorAlert();
             }
         }
     }
@@ -204,21 +210,9 @@ export class DbService {
     private writeAsync(path: string, str: string) {
         writeFile(path, str, { encoding: 'utf8', flag: 'w' }, (e) => {
             if (e) {
-                this.errorAlert();
+                DbService.errorAlert();
                 return;
             }
         });
-    }
-
-    private writeSync(path: string, str: string) {
-        writeFileSync(path, str, { encoding: 'utf8', flag: 'w' });
-    }
-
-    private errorAlert() {
-        alert('there is an error when we try to create ur databse');
-    }
-
-    private getSeparator() {
-        return os.platform() === 'win32' ? '\\' : '/';
     }
 }
